d419b7724d9daccf763116bf10890bf63e1cd4dc
mongsuk007 <34646538+mongsuk007@users.noreply.github.com>
2020-11-12 18:00:54 +0700

    Delete Mute Function

--- 12nov20.js
+++ 12nov20.js
@@ -25,10 +25,4 @@ function openPage(pageName, elmnt, color) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
-function mute(){
-
-  iframe.contentWindow.postMessage('{"method":"setVolume", "value":0}','*');
-
-}
   
