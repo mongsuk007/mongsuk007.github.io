024056df86451f0b9cd4200ccdd722a2b0fc1427
mongsuk007 <34646538+mongsuk007@users.noreply.github.com>
2021-07-30 16:11:30 +0700

    test-v3-1

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+// templatemo 467 easy profile
+
+// PRELOADER
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		 "images/tm-bg-slide-1.jpg", 
+	 		 "images/tm-bg-slide-2.jpg",
+             "images/tm-bg-slide-3.jpg",
+             "images/tm-bg-slide-4.jpg"
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
+
+// CLICK IMAGE TO ROTATE FOR FUN :D
+// Thanks to Arun https://stackoverflow.com/questions/16641619/javascript-click-to-rotate-image-infinitely-and-click-again-to-stop
+$(function() {
+    
+    var $rota = $('#profile_img')
+	
+	//.click is deprecated, use .on and 'click'
+    $rota.on('click',function(){
+        var $this = $(this);
+        
+        if($this.data('rotating')){
+            clearInterval($this.data('rotating'));
+            $this.data('rotating', false)
+        } else {
+            $this.data('rotating', setInterval(function(){
+                var degree = $this.data('degree') || 0;
+                $this.css({ transform: 'rotate(' + degree + 'deg)'});
+                $this.data('degree', ++degree)
+            }, 5));
+        }
+    });
+    
+});
